5383c13b0809b68bee92f2ff5bfd5ffe0e5e4bd7
body2w <90206564+body2w@users.noreply.github.com>
2025-06-07 23:47:15 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,25 @@ const monitor = document.querySelector(".monitor");
 var arr = [];
 btns.forEach((element) => {
   element.addEventListener("click", () => {
-    if (element.innerHTML != "=" & element.innerHTML != "AC") {
+    if ((element.innerHTML != "=") & (element.innerHTML != "AC")) {
       arr.push(element.innerHTML);
+      if (arr[0] == 0) {
+        arr.pop();
+      }
       monitor.innerHTML = arr.join("");
     } else if (element.innerHTML == "=") {
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == "+" || arr[i] == "-" || arr[i] == "*" || arr[i] == "/") {
+          if (arr[i + 1] == 0) {
+            arr[i + 1] = "";
+            console.log(arr);
+          }
+        }
+      }
       monitor.innerHTML = eval(arr.join(""));
       arr = [eval(arr.join(""))];
     } else if (element.innerHTML == "AC") {
-      monitor.innerHTML = " ";
+      monitor.innerHTML = "";
       arr = [];
     }
   });
